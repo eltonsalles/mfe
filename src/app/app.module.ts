7fcb3bbdda53c4262eb27eabc7fa0c1899da5ecd
Elton Leite <eltonrms.leite@hotmail.com>
2024-04-25 08:24:50 -0300

    feat: config para elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { createCustomElement } from "@angular/elements";
 
 import { AppComponent } from './app.component';
+import {MyComponentModule} from 'component-library';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyComponentModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('app-mfe', el);
+  }
+}
